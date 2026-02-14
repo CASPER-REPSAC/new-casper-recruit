02ddeecc142f4c1cb620ff0b0ea33936be4c79d5
장원지 <j06061379@gmail.com>
2026-02-14 22:55:22 +0900

    feat: update recruit deadline and form (2026)

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const APPLICATION_FORM_URL =
-  'https://docs.google.com/forms/d/e/1FAIpQLSfmfvklKP3JEoBBUAWFKYO2Zvte37t9pbzOhpPQOXVzoHZY_w/viewform';
+  'https://docs.google.com/forms/d/e/1FAIpQLScmwD13_WFtAP8D117MvDGtA1GurN42bpx_vOwU8v8O8P7mSA/viewform';
 
-export const DEADLINE = '2025. 03. 12.  까지';
+export const DEADLINE = '2026. 03. 16.  까지';
 
 export const REQUIREMENTS =
   '국립창원대학교 컴퓨터공학과·정보통신공학과 1~2학년 / 사림아너스학부·공학계열 자율전공학부';
